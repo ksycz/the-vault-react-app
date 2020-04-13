da01bde14add3ad84926170a4080df615785404d
ksycz <katarzynasycz@gmail.com>
2020-04-13 17:30:24 +0200

    Removed default stories and updated README

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
-import { GlobalStyle } from '../src/theme/GlobalStyle';
 import React from 'react';
+import { GlobalStyle } from '../src/theme/GlobalStyle';
 import { configure, addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/theme/mainTheme';
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # the-vault-react-app
+
+The app I create based on this course: https://eduweb.pl/programowanie-i-www/reactjs/react-w-praktyce
